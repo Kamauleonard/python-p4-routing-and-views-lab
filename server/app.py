616c830776b234296110868013ee8e3c7609aef4
Kamauleonard <leonardmwangi2012@gmail.com>
2024-01-22 15:26:21 +0300

    Initial Commit

--- server/app.py
+++ server/app.py
@@ -1,8 +1,37 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/hello')
+def print_hello():
+    text = 'hello'
+    print(text)  # Print to console
+    return text  # Return the plain text
+
+@app.route('/count/<int:number>')
+def count(number):
+    numbers_list = '\n'.join(str(i) for i in range(number + 1))
+    return numbers_list  # Return plain text
+
+@app.route('/math/<float:num1><operation><float:num2>')
+def math(num1, operation, num2):
+    result = None
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+
+    return str(result)  # Return the result as plain text
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
